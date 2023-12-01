99714a3b736384646a7a9425f9c05e677ecf49b7
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-01 04:25:28 -0500

    feat: add users router to app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sequelize = require('./config/database');
-const { DataTypes } = require('sequelize');
-
+const usersRouter = require('./controller/user');
 const app = express();
 
 // Use body-parser middleware to parse JSON and urlencoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// home
-app.get('/', (req, res) => {
-  res.send('Welcome to the home page!');
-});
+// routes
+app.use('/api/users', usersRouter);
 
 module.exports = app;
