8f7a3ae7d179803835ebef9b30e1bdf88b6826f9
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-02 01:23:49 -0500

    feat: add sequelize config and migration folder

--- src/config/config.js
+++ src/config/config.js
@@ -10,10 +10,10 @@ let DB_PORT;
 
 if (process.env.NODE_ENV === 'test') {
   DB_NAME = process.env.DB_NAME_TEST;
-  DB_USER = process.env.DB_USER_TEST;
-  DB_PASSWORD = process.env.DB_PASSWORD_TEST;
-  DB_HOST = process.env.DB_HOST_TEST;
-  DB_PORT = process.env.DB_PORT_TEST;
+  DB_USER = process.env.DB_USER;
+  DB_PASSWORD = process.env.DB_PASSWORD;
+  DB_HOST = process.env.DB_HOST;
+  DB_PORT = process.env.DB_PORT;
 } else {
   DB_NAME = process.env.DB_NAME;
   DB_USER = process.env.DB_USER;
