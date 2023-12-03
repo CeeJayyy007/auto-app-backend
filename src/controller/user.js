0fcdf19505187f051b87f90c219eb4d381b24a64
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-02 22:27:10 -0500

    refactor: update files

--- src/controller/user.js
+++ src/controller/user.js
@@ -7,89 +7,73 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 // Create a new user
 usersRouter.post('/', async (req, res) => {
-  try {
-    // Hash the password before storing it
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  // Hash the password before storing it
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-    // Create a new user with the hashed password
-    const newUser = await User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-      phone: req.body.phone,
-      avatar: null // Set the avatar to null for now (add default avatar later)
-    });
+  // Create a new user with the hashed password
+  const newUser = await User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+    phone: req.body.phone,
+    avatar: null // Set the avatar to null for now (add default avatar later)
+  });
 
-    // Exclude the password field from the response
-    const userWithoutPassword = newUser.toJSON();
-    delete userWithoutPassword.password;
+  // Exclude the password field from the response
+  const userWithoutPassword = newUser.toJSON();
+  delete userWithoutPassword.password;
 
-    res.status(201).json(userWithoutPassword);
-  } catch (error) {
-    next(error);
-  }
+  res.status(201).json(userWithoutPassword);
 });
 
 // Get all users
 usersRouter.get('/', authMiddleware.userExtractor, async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
+  const users = await User.findAll();
+  res.status(200).json(users);
 });
 
 // Get a specific user by ID
 usersRouter.get('/:userId', authMiddleware.userExtractor, async (req, res) => {
-  try {
-    const user = req.user;
+  const user = req.user;
 
-    if (!user) {
-      res.status(404).json({ error: 'User not found' });
-      return;
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
+  if (!user) {
+    res.status(404).json({ error: 'User not found' });
+    return;
   }
+  res.status(200).json(user);
 });
 
 // Update a user by ID
 usersRouter.put('/:userId', authMiddleware.userExtractor, async (req, res) => {
-  try {
-    const user = req.user;
+  const user = req.user;
 
-    if (!existingUser) {
-      res.status(404).json({ error: 'User not found' });
-      return;
-    }
-
-    // Hash the new password before updating (if provided)
-    if (req.body.password) {
-      req.body.password = await bcrypt.hash(req.body.password, 10);
-    }
-
-    // Update the user
-    const [updatedRows] = await User.update(req.body, {
-      where: { id: req.params.userId },
-      returning: true
-    });
-
-    if (updatedRows === 0) {
-      res.status(404).json({ error: 'User not found' });
-      return;
-    }
-
-    // Get the updated user record
-    const updatedUser = await User.findByPk(req.params.userId);
-
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    next(error);
+  if (!existingUser) {
+    res.status(404).json({ error: 'User not found' });
+    return;
   }
+
+  // Hash the new password before updating (if provided)
+  if (req.body.password) {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  // Update the user
+  const [updatedRows] = await User.update(req.body, {
+    where: { id: req.params.userId },
+    returning: true
+  });
+
+  if (updatedRows === 0) {
+    res.status(404).json({ error: 'User not found' });
+    return;
+  }
+
+  // Get the updated user record
+  const updatedUser = await User.findByPk(req.params.userId);
+
+  res.status(200).json(updatedUser);
 });
 
 // Delete a user by ID
@@ -97,21 +81,19 @@ usersRouter.delete(
   '/:userId',
   authMiddleware.userExtractor,
   async (req, res) => {
-    try {
-      const user = req.user;
+    const user = req.user;
 
-      const deletedRows = await User.destroy({
-        where: { id: user.id }
-      });
-      if (deletedRows === 0) {
-        res.status(404).json({ error: 'User not found' });
-        return;
-      }
-      res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    const deletedRows = await User.destroy({
+      where: { id: user.id }
+    });
+    if (deletedRows === 0) {
+      res.status(404).json({ error: 'User not found' });
+      return;
     }
+    res.status(200).json({ message: 'User deleted successfully' });
+
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 );
 
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,21 +3,19 @@ const chalk = require('chalk');
 
 // add error handler middleware
 const errorHandler = (error, request, response, next) => {
-  console.log(chalk.green('errorHandler.js error.name: ', error.name));
+  logger.error(error.message);
 
-  if (error instanceof CastError) {
+  if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
-  } else if (error instanceof ValidationError) {
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
-  } else if (error instanceof JsonWebTokenError) {
+  } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: error.message });
   } else if (error.name === 'TokenExpiredError') {
     return response.status(401).json({
       error: 'token expired'
     });
   }
-
-  logger.error(error.message);
 
   response.status(500).json({ error: 'Internal Server Error' });
 
