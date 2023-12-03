dc186910d0a491d4ae8c809fa9744cceeff579ad
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-03 01:09:33 -0500

    AA-33-Create-API-documentation (#7)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 require('express-async-errors');
 const bodyParser = require('body-parser');
-const usersRouter = require('./controller/user');
-const authRouter = require('./controller/auth');
+const authRouter = require('./routes/authRoutes');
+const usersRouter = require('./routes/userRoutes');
 const app = express();
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
@@ -10,24 +10,28 @@ const { tokenExtractor } = require('./middlewares/authMiddleware');
 const { morganMiddleware } = require('./config/logging');
 const unknownEndpoint = require('./middlewares/util');
 const morgan = require('morgan');
+const swaggerDocs = require('./swagger-ui/swagger');
 
+// middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('combine', { stream: morganMiddleware.stream }));
-
+app.disable('x-powered-by');
 app.use(tokenExtractor);
 
 // routes
+app.use('/api', authRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/', authRouter);
-app.use('/api/', authRouter);
 
 // middleware for testing purposes
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controller/testing');
   app.use('/api/testing', testingRouter);
 }
+
+// sawgger ui docs
+swaggerDocs(app);
 
 // use unknown endpoint middleware
 app.use(unknownEndpoint);
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,96 @@
+const authController = require('../controller/auth');
+const bodyParser = require('body-parser');
+const authRouter = require('express').Router();
+authRouter.use(bodyParser.json());
+
+// auth routes
+/**
+ * @openapi
+ * '/api/login':
+ *  post:
+ *     tags:
+ *     - User Controller
+ *     summary: Login as a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - username
+ *              - password
+ *            properties:
+ *              username:
+ *                type: string
+ *                default: johndoe
+ *              password:
+ *                type: string
+ *                default: johnDoe20!@
+ *     responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+authRouter.post('/login', authController.login);
+
+/** POST Methods */
+/**
+ * @openapi
+ * '/api/register':
+ *  post:
+ *     tags:
+ *     - User Controller
+ *     summary: Create a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *            type: object
+ *            required:
+ *              - firstName
+ *              - lastName
+ *              - username
+ *              - email
+ *              - password
+ *              - phone
+ *            properties:
+ *              firstName:
+ *                type: string
+ *                default: john
+ *              lastName:
+ *                type: string
+ *                default: doe
+ *              username:
+ *                type: string
+ *                default: johndoe
+ *              email:
+ *                type: string
+ *                default: johndoe@mail.com
+ *              password:
+ *                type: string
+ *                default: johnDoe20!@
+ *              phone:
+ *                type: number
+ *                default: +1234567890
+ *
+ *     responses:
+ *      201:
+ *        description: Created
+ *      409:
+ *        description: Conflict
+ *      404:
+ *        description: Not Found
+ *      500:
+ *        description: Server Error
+ */
+
+authRouter.post('/register', authController.register);
+
+module.exports = authRouter;
