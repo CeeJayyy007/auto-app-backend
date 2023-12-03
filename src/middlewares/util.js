437c33d393f4b1f0d7feb4f6614b55f68079056c
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-02 21:19:05 -0500

    refactor: add utils file to middleware for handling unknownendpoints

---
+++ src/middlewares/util.js
@@ -0,0 +1,6 @@
+// add unknown endpoint middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+module.exports = unknownEndpoint;
