e534c16a002eff76d358843fd609ca2dddacbb40
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-05 05:30:42 -0500

    fix: upate user model to exlude sensitive data

--- src/models/user.js
+++ src/models/user.js
@@ -2,48 +2,57 @@ const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 
 // Define the User model
-const User = sequelize.define('User', {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
+const User = sequelize.define(
+  'User',
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    avatar: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        is: /^\+?[1-9]\d{1,14}$/ // Regular expression to validate phone number format
+      }
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
     }
   },
-  avatar: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: true,
-    unique: true,
-    validate: {
-      is: /^\+?[1-9]\d{1,14}$/ // Regular expression to validate phone number format
+  {
+    // Exclude password field by default when converting to JSON
+    defaultScope: {
+      attributes: { exclude: ['password', 'deletedAt'] }
     }
-  },
-  deletedAt: {
-    type: DataTypes.DATE,
-    allowNull: true
   }
-});
+);
 
 module.exports = User;
