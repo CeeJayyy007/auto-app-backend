4c4e145a4e98915f5cf791fbc99a1f1c7fb696c2
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-08 01:28:24 -0500

    fix: fix migration file

--- migrations/20231208020549-update-appointments-table.js
+++ migrations/20231208020549-update-appointments-table.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.dropColumn('Services', 'serviceRequest', {
+    await queryInterface.removeColumn('Appointments', 'serviceRequest', {
       type: Sequelize.ARRAY(Sequelize.STRING),
       allowNull: false
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Services', 'serviceRequest', {
+    await queryInterface.addColumn('Appointments', 'serviceRequest', {
       type: Sequelize.ARRAY(Sequelize.STRING),
       allowNull: false
     });
