e0b71ead21c4836e4e105940c17daf701c2ea63b
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-25 18:13:44 -0500

    chore: update error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,26 +1,5 @@
 const { logger } = require('../config/logging');
 
-// add error handler middleware
-// const errorHandler = (error, request, response, next) => {
-//   logger.error(error.message);
-
-//   if (error.name === 'CastError') {
-//     return response.status(400).send({ error: 'malformatted id' });
-//   } else if (error.name === 'ValidationError') {
-//     return response.status(400).json({ error: error.message });
-//   } else if (error.name === 'JsonWebTokenError') {
-//     return response.status(401).json({ error: error.message });
-//   } else if (error.name === 'TokenExpiredError') {
-//     return response.status(401).json({
-//       error: 'token expired'
-//     });
-//   }
-
-//   response.status(500).json({ error: 'Internal Server Error' });
-
-//   next(error);
-// };
-
 const errorHandler = (error, _, res, __) => {
   // render the error page
   logger.log({
